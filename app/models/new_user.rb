7661f75ff8f497aa2130b69d53381b6dc3c48878
Hassan Siddiqui <hassansiddiqui03024053707@gmail.com>
2026-04-21 23:58:05 +0500

    Active Record Basics Complete, Active Record Migrations Partial

---
+++ app/models/new_user.rb
@@ -0,0 +1,9 @@
+class NewUser < ApplicationRecord
+  validates :name, presence: true 
+  after_create :log_new_user 
+
+  private
+    def log_new_user
+      puts "A new user was registered"
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_20_124216) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_21_165339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -50,6 +50,25 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_20_124216) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "book_orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "new_users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
