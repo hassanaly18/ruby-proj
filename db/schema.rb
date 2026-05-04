83c3e6f5d311941cf5a880580c575a54e2450346
Hassan Siddiqui <hassansiddiqui03024053707@gmail.com>
2026-05-05 00:00:54 +0500

    Post view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_28_111749) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_04_131752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -93,6 +93,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_28_111749) do
     t.datetime "created_at", null: false
     t.string "email"
     t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "context"
+    t.datetime "created_at", null: false
+    t.boolean "published"
+    t.string "title"
     t.datetime "updated_at", null: false
   end
 
